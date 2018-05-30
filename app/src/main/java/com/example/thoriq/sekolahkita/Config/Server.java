ea53f34a94a0d51fa4162c5e02f4f384b9bc1937
Thoriq29 <oiq2908@gmail.com>
2018-05-30 08:57:34 +0100

    test

--- app/src/main/java/com/example/thoriq/sekolahkita/Config/Server.java
+++ app/src/main/java/com/example/thoriq/sekolahkita/Config/Server.java
@@ -1,8 +1,8 @@
 package com.example.thoriq.sekolahkita.Config;
 
 public class Server {
-    public static final String SERVER_URL = "http://192.168.100.20/Profile/";
-    public static final String IMAGE_URL ="http://192.168.100.20/Profile/img/";
+    public static final String SERVER_URL = "http://192.168.100.21/Profile/";
+    public static final String IMAGE_URL ="http://192.168.100.21/Profile/img/";
     //http://thoriqcom.000webhostapp.com/Profile/
 }
 
